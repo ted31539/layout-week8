e41ac5e56ca1b90665c54410a250b573b7236275
Ted <Ted31539@gmail.com>
2021-08-30 00:46:09 +0800

    Update 2021-08-29T16:46:04.703Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,37 +1,38 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+"use strict";
 
-// 精心設計、課程多元
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // 精心設計、課程多元
+
 var swiper = new Swiper(".mySwiper-class", {
   slidesPerView: 2,
-  spaceBetween: 15,  
+  spaceBetween: 15,
   grid: {
     rows: 2,
-    fill: "row",
+    fill: "row"
   },
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 992px
     768: {
-      spaceBetween: 30,      
+      spaceBetween: 30
     },
     // when window width is >= 992px
     992: {
       slidesPerView: 1,
       grid: {
         rows: 1,
-        fill: "row",
-      },
-    },    
-  },    
-});
-// 聽聽他們怎麼說
+        fill: "row"
+      }
+    }
+  }
+}); // 聽聽他們怎麼說
+
 var swiper = new Swiper(".mySwiper-credit", {
   slidesPerView: 1,
   grid: {
     rows: 3,
-    fill: "row",
+    fill: "row"
   },
   // Responsive breakpoints
   breakpoints: {
@@ -40,21 +41,22 @@ var swiper = new Swiper(".mySwiper-credit", {
       slidesPerView: 2,
       grid: {
         rows: 2,
-        fill: "row",
-      },
+        fill: "row"
+      }
     },
     // when window width is >= 992px
     992: {
       slidesPerView: 3,
       grid: {
         rows: 2,
-        fill: "row",
-      },
-    },    
+        fill: "row"
+      }
+    }
   },
   spaceBetween: 28,
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    prevEl: ".swiper-button-prev"
+  }
 });
+//# sourceMappingURL=all.js.map
